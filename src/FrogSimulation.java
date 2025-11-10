3dd105912a9611a9eaa5dec4531f256a4fa01208
margauxk22 <margauxk2@nycstudents.net>
2025-11-10 11:45:39 -0500

    started frogsimulatio

---
+++ src/FrogSimulation.java
@@ -0,0 +1,13 @@
+public class FrogSimulation {
+    private int goalDistance{
+        int startingposition = 0;
+        int goal = 5;
+        goalDistance = startingposition - goal;
+
+    private int maxHops{
+        int hopDistance = 1;
+        maxHops = goalDistance / hopDistance;
+        }
+
+    }
+}
